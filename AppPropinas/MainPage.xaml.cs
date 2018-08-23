48809f5de1446ab936ff980f55194f81d49bbf11
goingtop <sebasipi501@gmail.com>
2018-08-23 14:17:12 -0500

    operacion echa

--- AppPropinas/MainPage.xaml.cs
+++ AppPropinas/MainPage.xaml.cs
@@ -17,9 +17,24 @@ namespace AppPropinas
 
       private void btnClicked(Object sender, EventArgs e)
         {
+            /* Convertir  a numero el total, propina, numero de personas*/
             double total = double.Parse(toti.Text);
             double propina = double.Parse(propi.Text);
             double numeropersona = double.Parse(numper.Text);
+            /* Se genera las operaciones previas */
+
+            double respropina = (total* propina) /100;
+            double restotal = respropina + total;
+            double rtapropinapersona = respropina/numeropersona;
+            double restotpersona = restotal / numeropersona;
+
+            txtpropina.Detail = propina.ToString("C");
+            txttotal.Detail = restotal.ToString("C");
+            txtpropersona.Detail = rtapropinapersona.ToString("C");
+            txttotalpersona.Detail = restotpersona.ToString("C");
+
+
+
 
         }
     }
